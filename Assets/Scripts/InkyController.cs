5e49efe45c0e06d4f864e03b23414855bfa73057
ColeMaunder <Cole@maunder.co.za>
2026-08-03 14:52:56 +0200

    Ghost warp and all ghosut aI

--- Assets/Scripts/InkyController.cs
+++ Assets/Scripts/InkyController.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class InkyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform pacTarget;
+    [SerializeField] private PacMovement pacMan;
+    [SerializeField] private Transform blinky;
     void Start()
     {
         
@@ -11,6 +13,21 @@ public class InkyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector2 pacOffset = Vector2.zero;
+        if(pacMan.direction == Vector2.up){
+            pacOffset = (Vector2)pacMan.transform.position + new Vector2(-2, 2);
+            
+        } else if(pacMan.direction == Vector2.right){
+            pacOffset = (Vector2)pacMan.transform.position + new Vector2(2, 0);
+            
+        } else if(pacMan.direction == Vector2.left){
+            pacOffset = (Vector2)pacMan.transform.position + new Vector2(-2, 0);
+            
+        } else if(pacMan.direction == Vector2.down){
+            pacOffset = (Vector2)pacMan.transform.position + new Vector2(0, -2);
+        } 
+        pacTarget.position = (Vector2)blinky.position + (pacOffset - (Vector2)blinky.position) * 2;
+
         
     }
 }
--- Assets/Scripts/PinkyController.cs
+++ Assets/Scripts/PinkyController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PinkyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private Transform targetPac;
+    [SerializeField] private PacMovement pacMan;
     void Update()
     {
-        
+        if(pacMan.direction == Vector2.up){
+            targetPac.position = (Vector2)pacMan.transform.position + new Vector2(-4, 4);
+            
+        } else if(pacMan.direction == Vector2.right){
+            targetPac.position = (Vector2)pacMan.transform.position + new Vector2(4, 0);
+            
+        } else if(pacMan.direction == Vector2.left){
+            targetPac.position = (Vector2)pacMan.transform.position + new Vector2(-4, 0);
+            
+        } else if(pacMan.direction == Vector2.down){
+            targetPac.position = (Vector2)pacMan.transform.position + new Vector2(0, -4);
+        } 
     }
 }
