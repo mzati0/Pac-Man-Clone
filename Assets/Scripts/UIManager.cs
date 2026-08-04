2d2d753aa0f0511ca847c4d29c24081fe985614a
Mzati (Laptop) <mzatichikoko@gmail.com>
2026-08-04 13:40:28 +0200

    Ui manager tweaks

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -23,6 +23,7 @@ public class UIManager : MonoBehaviour
     {
         GameManager.OnCreditChanged += OnCreditChanged;
         GameManager.OnScoreChanged += UpdateScore;
+        UpdateUICredits();
     }
     
     private void OnDisable()
@@ -52,7 +53,8 @@ public class UIManager : MonoBehaviour
     private void UpdateUICredits()
     {
         credits.SetDigit(0, digitSprites[_displayedCredits % 10]); // Ones place
-        credits.SetDigit(1, digitSprites[(_displayedCredits / 10) % 10]); // Tens place
+        credits.SetDigit(1, (_displayedCredits / 10) % 10 > 0 ? digitSprites[(_displayedCredits / 10) % 10] : null); // Clear tens place if it's zero
+        // Tens place
     }
     //int digit1 = (number / 100000) % 10;
     //int digit2 = (number / 10000) % 10;
@@ -69,8 +71,6 @@ public class UIManager : MonoBehaviour
         p2Score.SetDigit(0, digitSprites[GameManager.Instance.p2Score % 10]);
         
         highScore.SetDigit(0, digitSprites[GameManager.Instance.highScore % 10]);
-        
-
         
     }
 }
